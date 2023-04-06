a0aad770fc855fdc473ebc94e143c5280043f35f
林侑恆 <haleylin@apples-MacBook-Air.local>
2023-04-06 09:28:40 +0800

    init commit of shu 1976

---
+++ shu76_density_velocity.py
@@ -0,0 +1,43 @@
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ode(p, x):
+    
+    v = p[0]
+    a = p[1]
+
+    dvdx = (a*(x-v)-(2/x))*(x-v)/((x-v)**2-1)
+    dadx = (a*(a-(2/x)*(x-v))*(x-v))/((x-v)**2-1)
+
+    return [dvdx, dadx]
+#init condition
+p0 = [-5.44, 71.5]
+
+a = 0.2*1e5
+t = [1e12, 2e12, 4e12, 8e12]
+
+x = [i/20 for i in range(1, 20)]
+sol = odeint(ode, p0, x)
+
+r = [[i*a*t[0] for i in x], [i*a*t[1] for i in x], [i*a*t[2] for i in x], [i*a*t[3] for i in x]]
+v = sol[:, 0]
+u = [-(a*i) for i in v]
+alpha = sol[:, 1]
+rho = [[(i/(4*np.pi*6.67e-8*t[0]**2)) for i in alpha], [(i/(4*np.pi*6.67e-8*t[1]**2)) for i in alpha], [(i/(4*np.pi*6.67e-8*t[2]**2)) for i in alpha], [(i/(4*np.pi*6.67e-8*t[3]**2)) for i in alpha]]
+
+def zero_to_nan(values):
+    """Replace every 0 with 'nan' and return a copy."""
+    return [float('nan') if x==0 else x for x in values]
+
+plt.xlabel(r"$\log(r)$", fontsize=15)
+plt.ylabel(r"$\log(\rho)$", fontsize=15)
+#plt.ylabel(r"$\log(\mid u \mid)$", fontsize=15)
+for time in range(len(t)):
+    plt.plot(r[time], zero_to_nan(rho[time]), label=f"{t[time]/10**12}e12")
+    #plt.plot(r[time], u, label=f"{t[time]/10**12}e12")
+
+plt.yscale('log')
+plt.xscale('log')
+plt.legend()
+plt.show()
